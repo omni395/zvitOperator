d5d256bdc7072d69fd637ac71366dfd825b51d3c
Home <omni395@gmail.com>
2017-01-23 14:08:36 +0300

    pre for heroku

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,7 @@
 class Material < ApplicationRecord
   has_many :material_payments
   has_many :payments, through: :material_payments
+
+  validates :material_name, :material_price, precense: true, length: { maximum: 30 }
+  validates :material_price, numericality: { :greater_than_or_equal_to => 0 }
 end
