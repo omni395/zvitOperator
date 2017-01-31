b90cac1cc4b666c38b865b043c0e12d74cd36cca
Dimon <omni395@gmail.com>
2017-01-31 11:15:10 +0200

    checking if user is active and some troubles

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,27 +10,38 @@ class User < ApplicationRecord
   before_create :set_role
 
   # Валидации
-  validates :user_name, :first_name, :last_name, :middle_name, length: { maximum: 30 }
+  validates :user_name, :first_name, :last_name, :middle_name,
+            length: { maximum: 30 }
 
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   # НЕ ПЕРЕВОДИТ В НИЖНИЙ РЕГИСТР
 
-  # Переводим имя, фамилию и отчество пользователя в нижний регистра перед сохранением
+  # Переводим имя, фамилию и отчество пользователя
+  # в нижний регистр перед сохранением
   before_save :first_name.downcase
   before_save :last_name.downcase
   before_save :middle_name.downcase
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-# Проверка роли пользователя
+# Проверка роли пользователя и активности аккаунта
 def is_role?(role)
   self.role == Role.find_by_name(role)
+end
+
+def active_for_authentication?
+  super && self.active?
+end
+
+def inactive_message
+  :inactive
 end
   
   private
     def set_role
       if User.first == nil
         self.role = Role.find_by_name('admin')
+        self.active = true
       else
         self.role = Role.find_by_name('registered')
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131050750) do
+ActiveRecord::Schema.define(version: 20170131071811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20170131050750) do
     t.datetime "updated_at",                          null: false
     t.string   "user_name"
     t.integer  "role_id"
+    t.boolean  "active"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["role_id"], name: "index_users_on_role_id", using: :btree
